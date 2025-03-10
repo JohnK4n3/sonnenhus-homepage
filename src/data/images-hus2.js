f47b0c4cc51bf77109d8dfa26c4f193c13b7d467
Johannes <kane@macbookair.speedport.ip>
2025-03-10 01:11:07 +0100

    small images

--- src/data/images-hus2.js
+++ src/data/images-hus2.js
@@ -1,10 +1,10 @@
 let images = [
-    {id: 0, imagePath:"images/Hus2/Bild1.jpg"},
-    {id: 1, imagePath:"images/Hus2/Bild2.jpg"},
-    {id: 2, imagePath:"images/Hus2/Bild3.jpg"},
-    {id: 3, imagePath:"images/Hus2/Bild8.jpg"},
-    {id: 4, imagePath:"images/Hus2/Bild5.jpg"},
-    {id: 5, imagePath:"images/Hus2/Bild6.jpg"},
+    {id: 0, imagePath:"images/Hus2/Bild1_klein.jpg"},
+    {id: 1, imagePath:"images/Hus2/Bild2_klein.jpg"},
+    {id: 2, imagePath:"images/Hus2/Bild3_klein.jpg"},
+    {id: 3, imagePath:"images/Hus2/Bild8_klein.jpg"},
+    {id: 4, imagePath:"images/Hus2/Bild5_klein.jpg"},
+    {id: 5, imagePath:"images/Hus2/Bild6_klein.jpg"},
  
     
 ]; 
