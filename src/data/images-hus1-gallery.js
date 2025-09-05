d5a1e8ccc57beaf2e129b8a31a5061c5419b723f
JohnK4n3 <j_keve01@wwu.de>
2025-09-05 18:47:48 +0200

    new photos hus1

--- src/data/images-hus1-gallery.js
+++ src/data/images-hus1-gallery.js
@@ -1,14 +1,15 @@
 
 
 let images = [
-    {id: 0, imagePath:"images/Hus1/Bild1_klein.jpg"},
+    {id: 0, imagePath:"images/Hus1/Bild9_klein.jpg"},
     {id: 1, imagePath:"images/Hus1/Bild2_klein.jpg"},
-    {id: 2, imagePath:"images/Hus1/Bild3_klein.jpg"},
+    {id: 2, imagePath:"images/Hus1/Bild6_klein.jpg"},
     {id: 3, imagePath:"images/Hus1/Bild4_klein.jpg"},
-    {id: 4, imagePath:"images/Hus1/Bild5_klein.jpg"},
-    {id: 5, imagePath:"images/Hus1/Bild6_klein.jpg"},
-    {id: 6, imagePath:"images/Hus1/Bild7_klein.jpg"},
-    {id: 7, imagePath:"images/Hus1/Bild8_klein.jpg"},
+    {id: 4, imagePath:"images/Hus1/Bild8_klein.jpg"},
+    {id: 5, imagePath:"images/Hus1/Bild12_klein.jpg"},
+    {id: 6, imagePath:"images/Hus1/Bild10_klein.jpg"},
+    {id: 7, imagePath:"images/Hus1/Bild13_klein.jpg"},
+    {id: 8, imagePath:"images/Hus1/Bild11_klein.jpg"},
     // {id: 8, imagePath:"images/Hus1/Bild9.png"},
     // {id: 9, imagePath:"images/Hus1/Bild10.png"},
 ]; 
--- src/data/images-hus1.js
+++ src/data/images-hus1.js
@@ -1,10 +1,10 @@
 
 
 let images = [
-    {id: 0, imagePath:"images/Hus1/Bild1_klein.jpg"},
+    {id: 0, imagePath:"images/Hus1/Bild9_klein.jpg"},
     {id: 1, imagePath:"images/Hus1/Bild2_klein.jpg"},
-    {id: 2, imagePath:"images/Hus1/Bild5_klein.jpg"},
-    {id: 3, imagePath:"images/Hus1/Bild7_klein.jpg"},
+    // {id: 2, imagePath:"images/Hus1/Bild12_klein.jpg"},
+    {id: 3, imagePath:"images/Hus1/Bild13_klein.jpg"},
     
 ]; 
 export default images;
